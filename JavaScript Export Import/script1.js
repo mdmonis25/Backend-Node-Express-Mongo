9e404282e82cbddf5599deb89ad74dfc1671ccfd
Md <mdmonis128@gmail.com>
2024-04-04 00:06:59 +0530

    Add: Script 1

--- JavaScript Export Import/script1.js
+++ JavaScript Export Import/script1.js
@@ -1,2 +1,6 @@
-let b = require('./script2');
-console.log(b);
+// for single vlaues 
+// let b = require('./script2');
+let {first} = require('./script2');
+let {second} = require('./script2');
+console.log(first);
+console.log(second);
