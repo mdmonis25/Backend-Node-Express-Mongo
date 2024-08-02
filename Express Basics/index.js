5ecda8f71c097d912af1f9d7e07a45ddfa3871f1
Md <mdmonis128@gmail.com>
2024-08-02 14:44:19 +0530

    Express Basics  - > using Template Engines (ejs)

--- Express Basics/index.js
+++ Express Basics/index.js
@@ -1,29 +1,19 @@
-// Express is a framework of Node js which is a runtime environment for javascript
-// Node Js simply google a v8 engine hai jisko wrap kiya hai javascript me
-// V8 Javascript Runtime environment hai C++ code ka likha hua
-
 const express = require('express');
 const app = express();
 
-// Ye hai Middleware - ye server ke paas request jaane ke baad aur route chalne se pehle exucute hota hai
-// kuchh bhi execute hone se pehle middleware chalta hai 
 app.use(function(req,res,next){
-    res.send("Middleware hai ye");
+    // res.send("Middleware Running");
+    console.log("MiddleWare")
     next();
-})// middleware kitne bhi ho sakte hai 
-// request yahan ruk jaati hai, to usko route tak push karna padta hai (next wahi karta hai)
+})
 
-
-// get method route is used to get data from server, data is stored in req or request object
-// "/" is the default route
 app.get("/",(req,res)=>{
     res.send("Allahu Akbar");
 })
 
-// "/monis" is also a route
-// res is response from server
-app.get("/monis",(req,res)=>{
-    res.send("Monis ");
+
+app.get("/profile/:username",(req,res)=>{
+    res.send(`This is profile of : ${req.params.username}`);
 })
 
 app.listen(3000);
