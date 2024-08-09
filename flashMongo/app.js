abf8346b72f3b4ac256ea15afb1251388e5d0ae3
Md <mdmonis128@gmail.com>
2024-08-09 17:08:34 +0530

    flash Adeed

--- flashMongo/app.js
+++ flashMongo/app.js
@@ -15,6 +15,12 @@ var flash = require('connect-flash');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  resave:false,
+  saveUninitialized:false,
+  secret:"taqwaparhezgari"
+}))
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,11 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(session({
-  resave:false,
-  saveUninitialized:false,
-  secret:"taqwaparhezgari"
-}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- flashMongo/routes/index.js
+++ flashMongo/routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function(req, res) {
+  res.render('index');  
 });
+
+router.get('/failed',(req,res)=>{
+  req.flash("age",12);
+  req.flash("name","Monis");
+  res.send("Flash Created");
+})
+
+router.get('/check',(req,res)=>{
+  res.send("Check Now");
+  console.log(req.flash("age"), req.flash("name"));   
+})
+
 
 module.exports = router;
